85843b91c4e049388f1f56c23024d11b220fbdcc
jonarey <jack.reynolds96@gmail.com>
2020-10-26 19:55:23 -0700

    removed generic controller

---
+++ App.Sanguine/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Sanguine.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public int Get()
+        {
+            return 200;
+        }
+    }
+}
